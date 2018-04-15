543d2af3332afefd035a528aaf43d146835e7ad1
shafi hussain <shafi.globalempire@gmail.com>
2018-04-15 15:12:16 -0400

    Initial Commit

---
+++ prjREMAX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prjREMAX.Startup))]
+namespace prjREMAX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
