4889204ce5fd952873c18f0b4ebc1d886b5a1b93
shafi hussain <shafi.globalempire@gmail.com>
2018-04-15 18:39:50 -0400

    Created Add Property View

---
+++ prjREMAX/Controllers/PropertyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using prjREMAX.Models;
+using prjREMAX.ViewModels;
+
+namespace prjREMAX.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PropertyController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Property
+        public ActionResult Create()
+        {
+            var viewModel = new PropertyFormViewModel
+            {
+                Statuses = _context.Statuses.ToList(),
+                Types = _context.Types.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ prjREMAX/ViewModels/PropertyFormViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using prjREMAX.Models;
+
+namespace prjREMAX.ViewModels
+{
+    public class PropertyFormViewModel
+    {
+        //TextBoxes
+        public string PropertyName { get; set; }
+        public string Price { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Description { get; set; }
+        public string Year { get; set; }
+        public string CivicNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zipcode { get; set; }
+        public string Rooms { get; set; }
+        public string Bathrooms { get; set; }
+        public string Size { get; set; }
+        
+        //DropDown List
+        public int Type { get; set; }
+        public int Status { get; set; }
+        public IEnumerable<PropertyStatus> Statuses { get; set; }
+        public IEnumerable<PropertyType> Types { get; set; }
+    }
+}
